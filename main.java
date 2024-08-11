205dcaf71040393ba35dae258dec8ea2c563469b
Simon Tran <thanhnhantran1501@gmail.com>
2024-08-11 14:39:03 -0700

    20. Valid Parentheses

---
+++ main.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+//20. Valid Parentheses
+class main {
+  public boolean isValid(String s) {
+    HashMap<Character,Character> closings = new HashMap<Character,Character>();
+    closings.put(')','(');
+    closings.put('}','{');
+    closings.put(']','[');
+
+    Stack<Character> openings = new Stack<Character>();
+    for(int i=0;i<s.length();i++){
+    }
+
+    for (int i=0; i<s.length();i++){
+      if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{'){
+        openings.push(s.charAt(i));
+      }else if (openings.isEmpty() || openings.pop() != closings.get(s.charAt(i))) {
+        return false;
+      }
+    }
+    if(openings.isEmpty()){
+      return true;
+    }
+    return false;
+  }
+}
