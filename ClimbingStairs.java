77e560d66087b1eb1b454a133262063046a74f67
Simon Tran <thanhnhantran1501@gmail.com>
2024-08-21 16:33:00 -0700

    climbing stair tabulation and iterative solutions

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,5 +1,6 @@
 // 70. Climbing Stairs
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,4 +28,35 @@ class ClimbingStairs {
         }
         return memo.get(n);
     }
+    
+    public int climbStairsTabulation(int n) {
+      if(n==0||n==1){
+        return 1;
+      }
+
+      ArrayList<Integer> dp = new ArrayList<Integer>();
+      dp.add(1);
+      dp.add(1);
+
+      for(int i=2; i<=n; i++){
+          dp.add(dp.get(i-1)+dp.get(i-2)); 
+      }
+      return dp.get(n);
+    }
+
+    public int climbStairsIterative(int n) {
+      if(n<2){
+          return 1;
+      }
+
+      int prev=1;
+      int cur =1;
+
+      for(int i=2; i<=n; i++){
+          int temp = cur;
+          cur = prev + cur;
+          prev = temp;
+      }
+      return cur;
+  }
 }
