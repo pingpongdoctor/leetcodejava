5e724efdcb00914fe6054e1ecb2abc83282bf248
Simon Tran <thanhnhantran1501@gmail.com>
2024-09-02 12:44:13 -0700

    Search K smallest TreeNode using DFS inorder traversal solution

---
+++ KSmallestTreeNode.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class KSmallestTreeNode {
+  List<Integer> l = new ArrayList<Integer>();
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+  }
+  private void inorderTraverse(TreeNode root){
+      if(root==null){
+          return;
+      }
+      inorderTraverse(root.left);
+      l.add(root.val);
+      inorderTraverse(root.right);
+  }
+
+  private Integer getK(int k){
+      return this.l.get(k);
+  }
+
+  public int kthSmallest(TreeNode root, int k) {
+      inorderTraverse(root);
+      return getK(k-1);
+  }
+}
