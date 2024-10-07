9ca34b134fa57bbb1c0bf0cc8c681ea06501a3a8
Simon Tran <thanhnhantran1501@gmail.com>
2024-10-06 22:55:00 -0700

    NumberOfProvinces with BFS solution

--- NumberOfProvinces.java
+++ NumberOfProvinces.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 // 547. Number of Provinces
 //DFS and BFS have the same performance since we need them to traverse through all vertices
 class NumberOfProvinces {
-  // DFS solution
   private Map<Integer, List<Integer>> buildAdjList(int[][] isConnected) {
     Map<Integer, List<Integer>> map = new HashMap<>();
     for (int k = 0; k < isConnected.length; k++) {
@@ -34,7 +35,28 @@ class NumberOfProvinces {
     }
   };
 
-  public int findCircleNum(int[][] isConnected) {
+  private void bfs(Map<Integer, List<Integer>> adjList, Set<Integer> visitedCities, int r) {
+    // set up
+    Queue<Integer> queue = new LinkedList<Integer>();
+    queue.add(r);
+    visitedCities.add(r);
+    while (!queue.isEmpty()) {
+      int curSize = queue.size();
+      for (int i = 0; i < curSize; i++) {
+        int curRow = queue.poll();
+        for (int n : adjList.get(curRow)) {
+          if (visitedCities.contains(n)) {
+            continue;
+          }
+          queue.add(n);
+          visitedCities.add(n);
+        }
+      }
+    }
+  };
+
+  // DFS solution
+  public int NumberOfProvincesWithDFSSolution(int[][] isConnected) {
     Map<Integer, List<Integer>> adjList = buildAdjList(isConnected);
     Set<Integer> visitedCities = new HashSet<Integer>();
     int province = 0;
@@ -48,4 +70,16 @@ class NumberOfProvinces {
   }
 
   // BFS solution
+  public int NumberOfProvincesWithBFSSolution(int[][] isConnected) {
+    Map<Integer, List<Integer>> adjList = buildAdjList(isConnected);
+    Set<Integer> visitedCities = new HashSet<Integer>();
+    int province = 0;
+    for (int i = 0; i < isConnected.length; i++) {
+      if (!visitedCities.contains(i)) {
+        bfs(adjList, visitedCities, i);
+        province++;
+      }
+    }
+    return province;
+  }
 }
