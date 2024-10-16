d177fbc964c1c5c5ffb4061bd018cf47769fa328
Simon Tran <thanhnhantran1501@gmail.com>
2024-10-15 23:26:37 -0700

    review two pointers

--- Review.java
+++ Review.java
@@ -584,4 +584,38 @@ public class Review {
     }
     return minL;
   }
+
+  // Two pointers
+  // move two pointers to shrink the array from left and right
+  // use length method to return the length of string
+  // use charAt to get character from string using index
+  public static boolean isPalindrome(String word) {
+    int L = 0;
+    int R = word.length() - 1;
+    while (L < R) {
+      if (word.charAt(L) != word.charAt(R)) {
+        return false;
+      }
+      L++;
+      R--;
+    }
+    return true;
+  }
+
+  // Target sum
+  public static int[] targetSum(int[] nums, int target) {
+    int L = 0;
+    int R = nums.length - 1;
+    while (L < R) {
+      if (nums[L] + nums[R] < target) {
+        L++;
+      } else if (nums[L] + nums[R] > target) {
+        R--;
+      } else {
+        int[] result = { L, R };
+        return result;
+      }
+    }
+    return null;
+  }
 }
