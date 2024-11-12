4f8fb5d4219bda4106dec7cf94c9ca10e8856041
Simon Tran <thanhnhantran1501@gmail.com>
2024-11-11 18:04:19 -0800

    1071. Greatest Common Divisor of Strings Euclidean algorithm

--- GreatestCommonDivisorStrings.java
+++ GreatestCommonDivisorStrings.java
@@ -24,5 +24,24 @@ public class GreatestCommonDivisorStrings {
     }
     return rs;
   }
+
+  // optimal solution with Euclidean algorithm that is used for finding the
+  // greatest common division of two numbers
+  private int gcdLength(int a, int b) {
+    while (b != 0) {
+      int temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
+  }
+
+  public String optimalGcdOfStrings(String str1, String str2) {
+    int len = gcdLength(str1.length(), str2.length());
+    String subStr = str1.substring(0, len);
+    if (str1.equals(subStr.repeat(str1.length() / len)) && str2.equals(subStr.repeat(str2.length() / len))) {
+      return subStr;
+    }
+    return "";
+  }
 }
-// optimal solution
