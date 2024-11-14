27ae5ae9d0a0368ec3b80c2e245caa2b9f1b4339
Simon Tran <thanhnhantran1501@gmail.com>
2024-11-14 09:37:18 -0800

    443. String Compression two while loops solution

--- StringCompression.java
+++ StringCompression.java
@@ -1,35 +1,32 @@
 // 443. String Compression
+//use two while loops instead of one for loop to easily count the elements and effortlessly manage pointers
 public class StringCompression {
   class Solution {
     public int compress(char[] chars) {
-      StringBuilder s = new StringBuilder();
-      s.append(chars[0]);
-      int count = 0;
-      char cur = chars[0];
-      for (int i = 0; i < chars.length; i++) {
-        if (chars[i] == cur) {
+      int index = 0;
+      int cp = 0;
+      int i = 0;
+      while (i < chars.length) {
+        int count = 0;
+        while (i < chars.length && chars[i] == chars[cp]) {
           count++;
+          i++;
         }
 
-        if (chars[i] != cur || i == chars.length - 1) {
-          if (count > 1) {
-            s.append(count);
+        chars[index] = chars[cp];
+        cp = i;
+        index++;
+
+        if (count > 1) {
+          char[] letters = Integer.toString(count).toCharArray();
+          for (int j = 0; j < letters.length; j++) {
+            chars[index] = letters[j];
+            index++;
           }
-          s.append(chars[i]);
-          cur = chars[i];
-          count = 1;
-        }
-        if (i == chars.length - 1 && !Character.isDigit(chars[i])) {
-          s.deleteCharAt(s.length() - 1);
         }
       }
 
-      char[] ans = s.toString().toCharArray();
-      int len = ans.length;
-      for (int i = 0; i < len; i++) {
-        chars[i] = ans[i];
-      }
-      return len;
+      return index;
     }
   }
 }
