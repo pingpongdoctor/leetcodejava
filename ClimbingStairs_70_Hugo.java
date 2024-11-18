d68db202e61499ecbeb4c5f8b13ae1fc684d81a5
Simon Tran <thanhnhantran1501@gmail.com>
2024-11-17 18:10:45 -0800

    fix bugs

--- ClimbingStairs_70_Hugo.java
+++ ClimbingStairs_70_Hugo.java
@@ -1,26 +1,26 @@
-// 70. Climbing Stairs
 
-class Solution {
+// 70. Climbing Stairs
+import java.util.HashMap;
+import java.util.Map;
+
+class ClimbingStairs_70_Hugo {
     Map<Integer, Integer> track = new HashMap<>();
 
     public int climbStairs(int n) {
-        if (n == 1){
+        if (n == 1) {
             return 1;
-        }
-        else if (n == 2){
+        } else if (n == 2) {
             return 2;
         }
-        
-        if (track.containsKey(n)){
+
+        if (track.containsKey(n)) {
             return track.get(n);
-        }
-        else{
-            int currentSteps = climbStairs(n-1) + climbStairs(n-2);
+        } else {
+            int currentSteps = climbStairs(n - 1) + climbStairs(n - 2);
             track.put(n, currentSteps);
             return currentSteps;
         }
 
     }
-
 
 }
