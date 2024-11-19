295d1039572f59d212c8f3bacc82bb287467fee4
Simon Tran <thanhnhantran1501@gmail.com>
2024-11-18 16:32:01 -0800

    198. House Robber DP solution

--- HouseRobber.java
+++ HouseRobber.java
@@ -20,5 +20,22 @@ public class HouseRobber {
     return maxSum;
   }
 
-  // optimal solution with DP
+  // optimal solution with DP to calculate the base case of the last house
+  public int rob2(int[] nums) {
+    if (nums.length == 1) {
+      return nums[0];
+    }
+    if (nums.length == 2) {
+      return Math.max(nums[0], nums[1]);
+    }
+
+    int len = nums.length;
+    int[] maxSum = { Math.max(nums[len - 2], nums[len - 1]), nums[len - 1] };
+    for (int i = len - 3; i >= 0; i--) {
+      int temp = maxSum[0];
+      maxSum[0] = Math.max(maxSum[0], nums[i] + maxSum[1]);
+      maxSum[1] = temp;
+    }
+    return maxSum[0];
+  }
 }
