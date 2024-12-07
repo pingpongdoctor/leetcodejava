3013d59bb6f79c913da55d65673273507627a404
Simon Tran <thanhnhantran1501@gmail.com>
2024-12-06 17:15:49 -0800

    solution with max heap for 1268. Search Suggestions System

--- SearchSuggestionsSystem.java
+++ SearchSuggestionsSystem.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 // 1268. Search Suggestions System
 // Trie and backtracking solution
@@ -68,6 +70,65 @@ public class SearchSuggestionsSystem {
       prefix.append(c);
       List<String> words = returnWords(prefix.toString(), root);
       rs.add(words);
+    }
+    return rs;
+  }
+
+  // solution with max heap
+  class TrieNode2 {
+    TrieNode2[] children = new TrieNode2[26];
+    PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> b.compareTo(a));
+
+    public void addPq(String word) {
+      pq.add(word);
+      if (pq.size() > 3) {
+        pq.poll();
+      }
+    }
+
+    public List<String> getTopThree() {
+      List<String> ans = new ArrayList<>();
+      while (!pq.isEmpty()) {
+        ans.add(pq.poll());
+      }
+      Collections.reverse(ans);
+      return ans;
+    }
+  }
+
+  private void insert2(String word, TrieNode2 root) {
+    TrieNode2 cur = root;
+    for (char c : word.toCharArray()) {
+      if (cur.children[c - 'a'] == null) {
+        cur.children[c - 'a'] = new TrieNode2();
+      }
+      cur = cur.children[c - 'a'];
+      cur.addPq(word);
+    }
+  }
+
+  public List<List<String>> suggestedProducts2(String[] products, String searchWord) {
+    TrieNode2 root = new TrieNode2();
+    for (String word : products) {
+      insert2(word, root);
+    }
+    List<List<String>> rs = new ArrayList<List<String>>();
+    StringBuilder prefix = new StringBuilder();
+    for (char c : searchWord.toCharArray()) {
+      prefix.append(c);
+      TrieNode2 cur = root;
+      for (char charac : prefix.toString().toCharArray()) {
+        if (cur.children[charac - 'a'] == null) {
+          cur = null;
+          break;
+        }
+        cur = cur.children[charac - 'a'];
+      }
+      if (cur != null) {
+        rs.add(cur.getTopThree());
+      } else {
+        rs.add(new ArrayList<>());
+      }
     }
     return rs;
   }
