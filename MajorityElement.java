629d5dd22714ec46c7b3e550b1686843e82c1ba9
Simon Tran <thanhnhantran1501@gmail.com>
2024-12-12 00:26:09 -0800

    169. Majority Element hashmap solution

---
+++ MajorityElement.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+//169. Majority Element
+//hashmap solution
+public class MajorityElement {
+  public int majorityElement(int[] nums) {
+    int times = nums.length / 2;
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int n : nums) {
+      if (!map.containsKey(n)) {
+        map.put(n, 1);
+      } else {
+        map.put(n, map.get(n) + 1);
+      }
+    }
+    int rs = nums[0];
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      int key = entry.getKey();
+      int val = entry.getValue();
+      if (val > times) {
+        rs = val;
+      }
+    }
+    return rs;
+  }
+}
