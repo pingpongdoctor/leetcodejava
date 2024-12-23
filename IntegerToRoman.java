803ff95765028d9ba9f27f4beeb24ba2c975cd89
Simon Tran <thanhnhantran1501@gmail.com>
2024-12-23 00:53:53 -0800

    12. Integer to Roman math solution

---
+++ IntegerToRoman.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+// 12. Integer to Roman
+// Math solution
+public class IntegerToRoman {
+  public String intToRoman(int num) {
+    Map<Character, Integer> map = new HashMap<>();
+    int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    StringBuilder rs = new StringBuilder();
+    int i = 0;
+    while (i < values.length) {
+      while (i < values.length - 1 && num < values[i]) {
+        i++;
+      }
+      while (num >= values[i]) {
+        rs.append(symbols[i]);
+        num -= values[i];
+      }
+      i++;
+    }
+    return rs.toString();
+  }
+}
