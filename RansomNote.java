d7080f6fd501663064f94e769408b40dafc31226
Simon Tran <thanhnhantran1501@gmail.com>
2025-01-10 08:23:31 +0700

    // 383. Ransom Note map solution

--- RansomNote.java
+++ RansomNote.java
@@ -1,9 +1,23 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
+// 383. Ransom Note
+// use map to track characters and number of characters
 public class RansomNote {
-  public boolean canConstruct() {
-    Set<String> set = new HashSet<>();
-    return false;
+  public boolean canConstruct(String ransomNote, String magazine) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (char c : magazine.toCharArray()) {
+      map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+
+    for (char c1 : ransomNote.toCharArray()) {
+      if (map.containsKey(c1) && map.get(c1) > 0) {
+        map.put(c1, map.get(c1) - 1);
+      } else {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
