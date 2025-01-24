0440b0d26fca1ce9dc1cf2a539392c9d561a0ed0
Simon Tran <thanhnhantran1501@gmail.com>
2025-01-24 20:25:50 +0700

    fix the 452. Minimum Number of Arrows to Burst Balloons

--- MinimumNumberArrowsBurstBalloons.java
+++ MinimumNumberArrowsBurstBalloons.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 
 // 452. Minimum Number of Arrows to Burst Balloons
-// Use lastend to track the back of the last interval and check if it overlaps with the current interval
+// Sort the intervals by the end values
+// Use the end value of the previous range to track the back of the last interval and check if it overlaps with the current interval
 public class MinimumNumberArrowsBurstBalloons {
   public int findMinArrowShots(int[][] points) {
     if (points.length == 1) {
@@ -12,11 +13,9 @@ public class MinimumNumberArrowsBurstBalloons {
     int exploded = 1;
     int lastEnd = points[0][1];
     for (int i = 1; i < points.length; i++) {
-      if (lastEnd <= points[i][1] && lastEnd >= points[i][0]) {
-        continue;
-      } else {
-        lastEnd = points[i][1];
+      if (points[i][0] > lastEnd) {
         exploded++;
+        lastEnd = points[i][1];
       }
     }
     return exploded;
