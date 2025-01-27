84e613917fa487d1884b366a95e49a349aaa16c1
Simon Tran <thanhnhantran1501@gmail.com>
2025-01-27 15:24:44 +0700

    155. Min Stack 2 stacks solution

--- MinStack.java
+++ MinStack.java
@@ -1,43 +1,34 @@
 import java.util.Stack;
 
 // 155. Min Stack
-
 class MinStack {
-  Stack<Integer> min= new Stack<Integer>();
+  Stack<Integer> min = new Stack<Integer>();
   Stack<Integer> stack = new Stack<Integer>();
 
   public MinStack() {
   }
 
   public void push(int val) {
-      stack.push(val);
-        if(min.isEmpty()==false||val<min.peek()){
-          min.push(val);
-      }
+    stack.push(val);
+    if (min.isEmpty() == false || val <= min.peek()) {
+      min.push(val);
+    }
   }
-  
+
   public void pop() {
-      if(stack.size()!=0){
-        if (stack.pop()==min.peek()){
-          min.pop();
-        }
+    if (!stack.isEmpty()) {
+      int removedValue = stack.pop();
+      if (removedValue == min.peek()) {
+        min.pop();
       }
+    }
   }
-  
+
   public int top() {
-      return stack.peek();
+    return stack.peek();
   }
-  
+
   public int getMin() {
-      return min.peek();
+    return min.peek();
   }
 }
-
-/**
-* Your MinStack object will be instantiated and called as such:
-* MinStack obj = new MinStack();
-* obj.push(val);
-* obj.pop();
-* int param_3 = obj.top();
-* int param_4 = obj.getMin();
-*/
