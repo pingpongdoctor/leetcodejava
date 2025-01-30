2b24d316c7cbfe92359c80b6b659b3ef26618077
Simon Tran <thanhnhantran1501@gmail.com>
2025-01-30 21:50:54 +0700

    92. Reverse Linked List II

---
+++ ReverseLinkedList2.java
@@ -0,0 +1,38 @@
+// 92. Reverse Linked List II
+public class ReverseLinkedList2 {
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  public ListNode reverseBetween(ListNode head, int left, int right) {
+    ListNode dummyNode = new ListNode();
+    dummyNode.next = head;
+    ListNode prev = dummyNode;
+    for (int i = 0; i < left - 1; i++) {
+      prev = prev.next;
+    }
+    ListNode cur = prev.next;
+
+    for (int j = 1; j <= right - left; j++) {
+      ListNode temp = cur.next;
+      cur.next = temp.next;
+      temp.next = prev.next;
+      prev.next = temp;
+    }
+
+    return dummyNode.next;
+  }
+}
