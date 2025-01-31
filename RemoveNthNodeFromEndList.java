d2c624fc24118d73119798d45f419adcffde20f5
Simon Tran <thanhnhantran1501@gmail.com>
2025-01-31 23:56:46 +0700

    19. Remove Nth Node From End of List two pointers solution

--- RemoveNthNodeFromEndList.java
+++ RemoveNthNodeFromEndList.java
@@ -33,10 +33,33 @@ public class RemoveNthNodeFromEndList {
       p = p.next;
       i++;
     }
+
     int idx = i - n;
     ListNode bf = map.get(idx - 1);
     ListNode af = map.get(idx + 1);
     bf.next = af;
+    return dummyNode.next;
+  }
+
+  // Two pointers
+  public ListNode removeNthFromEnd2(ListNode head, int n) {
+    if (head == null || head.next == null) {
+      return null;
+    }
+    ListNode dummyNode = new ListNode();
+    dummyNode.next = head;
+    ListNode slow = dummyNode;
+    ListNode fast = dummyNode;
+
+    while (n > 0) {
+      fast = fast.next;
+      n--;
+    }
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+    slow.next = slow.next.next;
     return dummyNode.next;
   }
 }
