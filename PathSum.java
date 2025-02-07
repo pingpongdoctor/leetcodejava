212202d3c627e7b6707cd23c978d4a7807f2834d
Simon Tran <thanhnhantran1501@gmail.com>
2025-02-07 14:15:37 +0700

    112. Path Sum update

--- PathSum.java
+++ PathSum.java
@@ -1,16 +1,28 @@
+// 112. Path Sum
+// DFS solution
+// Check if sum equals to target and the current node is the leaf node to return true
 class PathSum {
-  public boolean dfs(TreeNode root, int targetSum, int curSum) {
-      if(root==null){
-          return false;
-      }
-      curSum=curSum+root.val;
-      if(root.left==null&&root.right==null&&curSum==targetSum){
-          return true;
-      }
-      return dfs(root.left, targetSum, curSum)||dfs(root.right, targetSum, curSum);
-  }
+    private boolean dfs(TreeNode node, int targetSum, int sum) {
+        if (node == null) {
+            return false;
+        }
 
-  public boolean hasPathSum(TreeNode root, int targetSum) {
-      return dfs(root,targetSum,0);
-  }
+        sum += node.val;
+        if (sum == targetSum && node.left == null && node.right == null) {
+            return true;
+        }
+        return dfs(node.left, targetSum, sum) || dfs(node.right, targetSum, sum);
+
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+
+        return dfs(root, targetSum, 0);
+    }
 }
