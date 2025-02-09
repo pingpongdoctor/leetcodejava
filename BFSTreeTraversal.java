69fe2bab5689b1f13aea6563d27f9b2994873457
Simon Tran <thanhnhantran1501@gmail.com>
2025-02-09 15:43:14 +0700

    102. Binary Tree Level Order Traversal update

--- BFSTreeTraversal.java
+++ BFSTreeTraversal.java
@@ -2,32 +2,41 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+
 // 102. Binary Tree Level Order Traversal
 class BFSTreeTraversal {
-  public List<List<Integer>> levelOrder(TreeNode root) {
-      List<List<Integer>> list = new ArrayList<List<Integer>>();
-      Queue<TreeNode> queue = new LinkedList<TreeNode>();
-      if(root!=null){
-          queue.add(root);
-      }
-      while(!queue.isEmpty()){
-          int levelLength = queue.size();
-          List<Integer> curLevelNodeVals = new ArrayList<Integer>();
-          for(int i = 0; i<levelLength; i++){
-              TreeNode cur = queue.remove();
-              curLevelNodeVals.add(cur.val);
-              if(cur.left!=null){
-                  queue.add(cur.left);
-              }
-              if(cur.right!=null){
-                  queue.add(cur.right);
-              }
-          }
-          if(curLevelNodeVals.isEmpty()==false){
-              list.add(curLevelNodeVals);
-          }
-          
-      }
-      return list;
-  }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        if (root.left == null && root.right == null) {
+            List<Integer> newList = new ArrayList<>();
+            newList.add(root.val);
+            ans.add(newList);
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            int size = queue.size();
+            List<Integer> newList = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                newList.add(cur.val);
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+
+            ans.add(newList);
+        }
+
+        return ans;
+    }
 }
