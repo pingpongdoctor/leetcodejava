b24e753e1f004346a211b88b952d7b9a79d1a2f4
Simon Tran <simontran@Simons-MacBook-Pro.local>
2025-02-25 00:54:48 -0800

    108. Convert Sorted Array to Binary Search Tree update

--- ConvertSortedArrayBinarySearchTree.java
+++ ConvertSortedArrayBinarySearchTree.java
@@ -1,10 +1,11 @@
 // 108. Convert Sorted Array to Binary Search Tree
+// DFS to build the mid node and return it at every level of the tree
 public class ConvertSortedArrayBinarySearchTree {
     private TreeNode helper(int[] nums, int left, int right){
         if (left > right){
             return null;
         }
-        int mid = (left + right) / 2;
+        int mid = left +  (right - left) / 2;
         
         TreeNode root = new TreeNode(nums[mid]);
         root.left = helper(nums, left, mid - 1);
