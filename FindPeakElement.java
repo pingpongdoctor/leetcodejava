2527168a47261de1df9def03430bb7801c4fd5f7
pingpongdoctor <thanhnhantran1501@gmail.com>
2025-03-01 12:07:00 -0800

    162. Find Peak Element binary search

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -2,41 +2,25 @@
 //Binary search
 public class FindPeakElement {
   public int findPeakElement(int[] nums) {
-    if (nums.length == 1) {
-      return 0;
-    }
-    if (nums.length == 2) {
-      return nums[0] > nums[1] ? 0 : 1;
+    if(nums.length==1){
+        return 0;
     }
     int L = 0;
-    int R = nums.length - 1;
-    int rs = 0;
-
-    while (L <= R) {
-      int mid = (R + L) / 2;
-      int cur = nums[mid];
-      // first or last index
-      if ((mid == 0 && cur > nums[mid + 1]) || (mid == nums.length - 1 && cur > nums[mid - 1])) {
-        rs = mid;
-        break;
-      }
-
-      // index in the middle
-      if (cur > nums[mid - 1] && cur > nums[mid + 1]) {
-        rs = mid;
-        break;
-      }
-
-      // shrinking the window
-      if (cur < nums[mid - 1]) {
-        R = R - 1;
-        continue;
-      }
-      if (cur < nums[mid + 1]) {
-        L = L + 1;
-        continue;
-      }
+    int R = nums.length-1;
+    
+    while(L<=R){
+        int mid = L + (R-L)/2;
+        int cur = nums[mid];
+        int before = mid==0 ? cur-1 : nums[mid-1];
+        int after = mid==nums.length-1 ? cur-1 : nums[mid+1];
+        if(cur>before && cur>after){
+            return mid;
+        } else if(cur<=after){
+            L = mid+1;
+        } else if(cur<=before){
+            R = mid-1;
+        }
     }
-    return rs;
-  }
+    return -1;
+}
 }
