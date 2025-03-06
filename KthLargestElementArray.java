e906406e7c2e1a5b4c8f176ece50c0b0d251fa31
pingpongdoctor <thanhnhantran1501@gmail.com>
2025-03-05 23:51:27 -0800

    34. Find First and Last Position of Element in Sorted Array update

--- KthLargestElementArray.java
+++ KthLargestElementArray.java
@@ -1,6 +1,7 @@
 import java.util.PriorityQueue;
 
 // 215. Kth Largest Element in an Array
+// min heap solution
 public class KthLargestElementArray {
         public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
