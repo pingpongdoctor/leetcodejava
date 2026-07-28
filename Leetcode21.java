f426d67f84884863dba827ba7f00daa4a3dc9dc6
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-07-28 00:02:05 -0700

    leetcode 21 merge two sorted lists

---
+++ Leetcode21.java
@@ -0,0 +1,63 @@
+package dsaAdvanced;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+
+Solution 1: Dummy node
+
+while
+ 4
+ i
+
+ 3 -> 4
+ j
+
+dummynode -> 1 -> 1 -> 2 -> 2
+                           tail
+T O(m + n)
+S O(m + n)
+
+
+ */
+public class Leetcode21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) {
+            return list2;
+        }
+
+        if(list2 == null) {
+            return list1;
+        }
+
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        if(list1 != null) {
+            tail.next = list1;
+        }
+
+        if(list2 != null) {
+            tail.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
