2bf005ca2fd2d3a5ed183c0cd8178c2e9b45e5b9
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-09-20 18:23:40 -0700

    Longest Palindromic Substring expand fromt he center

--- Leetcode5.java
+++ Leetcode5.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
 Solution 1: Brute force. Generate all possible substring of the input and check if they are palindrom
 0   1   2   3   4
@@ -58,5 +61,54 @@ public class Leetcode5 {
         }
 
         return s.substring(start, end + 1);
+    }
+
+    public String longestPalindrome2(String s) {
+        int max = Integer.MIN_VALUE;
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // Evalulate the case in which the palindrom has an even number of character
+            int l = i;
+            int r = i;
+
+            while (l - 1 >= 0 && r + 1 < s.length()) {
+                if(s.charAt(l-1) == s.charAt(r+1)) {
+                    l--;
+                    r++;
+                } else {
+                    break;
+                }
+            }
+
+            if (r - l + 1 > max) {
+                max = r - l + 1;
+                start = l;
+                end = r;
+            }
+
+            int l2 = i;
+            int r2 = i + 1;
+
+            if (r2 < s.length() && s.charAt(r2) == s.charAt(l2)) {
+                while (l2 - 1 >= 0 && r2 + 1 < s.length()) {
+                    if(s.charAt(l2-1) == s.charAt(r2+1)) {
+                        l2--;
+                        r2++;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (r2 - l2 + 1 > max) {
+                    max = r2 - l2 + 1;
+                    start = l2;
+                    end = r2;
+                }
+            }
+        }
+
+        return s.substring(start, end+1);
     }
 }
